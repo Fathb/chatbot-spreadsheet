f18d1da687194d1c828b3f81b1aef2e94afd9bf0
fathb <eglass.boy@gmail.com>
2021-11-17 09:07:36 +0700

    menambah command daftar

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const {MessageType, WAConnection } = require('@adiwajshing/baileys');
+const {
+ MessageType,
+ WAConnection
+} = require('@adiwajshing/baileys');
 const fs = require('fs');
 
 async function connectToWhatsApp () {
-    const conn = new WAConnection();
-    await conn.loadAuthInfo ('./auth_info.json');
-    await conn.connect ();
-    conn.on("chat-update", chat=>{
-        if(!chat.hasNewMessage) return;
-        const msg = chat.messages.all()[0];
-        if (msg.key.fromMe) return;
-        if (msg.message.conversation == "daftar") {
-           conn.sendMessage(msg.key.remoteJid, "selamat anda terdaftar", MessageType.text); 
-        };
-    })
+ const conn = new WAConnection();
+ await conn.loadAuthInfo ('./auth_info.json');
+ await conn.connect ();
+ conn.on("chat-update", chat=> {
+  if (!chat.hasNewMessage) return;
+  const msg = chat.messages.all()[0];
+  if (msg.key.fromMe) return;
+  const commands = msg.message.conversation.split(' ')[0];
+  let options;
+  const args = msg.message.conversation.split(' ')[2];
+
+  if (commands == "daftar") {
+   const userBuffer = fs.readFileSync('data/user.json', 'utf8');
+   options = msg.message.conversation.split('-')[1];
+   const data = {
+    remoteJid: msg.key.remoteJid,
+    nama: options
+   }
+   const user = JSON.parse(userBuffer);
+   console.log(user);
+   user.push(data);
+   fs.writeFileSync('data/user.json', JSON.stringify(user));
+   conn.sendMessage(msg.key.remoteJid, `terima kasih! ${options} telah terdaftar di sistem kami.`, MessageType.text);
+  }
+  // if (msg.message.conversation == "list") {
+  //  conn.sendMessage(msg.key.remoteJid, button, MessageType.listMessage);
+  // }
+  // if (msg.message.conversation == "button") {
+  //  conn.sendMessage(msg.key.remoteJid, buttonMessage, MessageType.buttonsMessage);
+  // }
+ })
 }
 // run in main file
 connectToWhatsApp ()
-.catch (err => console.log("unexpected error: " + err) ) // catch any errors
+.catch (err => console.log("unexpected error: " + err)) // catch any errors
