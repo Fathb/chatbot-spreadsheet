93afc772783360ede5a038b1bd808e16e3dfda07
fathb <eglass.boy@gmail.com>
2022-04-04 09:10:56 +0700

    create md

---
+++ appScript.js
@@ -0,0 +1,19 @@
+// const fetch = require("node-fetch")
+// function getPdfURL(namafile) {
+//   let url;
+//   try {
+//     fetch('https://script.google.com/macros/s/AKfycbzs8gPbSb-23e4ERJXZev0GsQdBIGfAzDw_bAThL_RgqfOtJu0syQ7iIlCmvzKZFowNfQ/exec?namafile='+namafile, {
+//       method: 'post'
+//     }).then(res=>res.json()).then(res=> {
+//       url = res;
+//       console.log(url);
+//     })
+//   } catch (e) {
+//     console.log(e);
+//   }
+//   return url
+// }
+// console.log(getPdfURL("202204Yaqdan siroj"));
+// module.export = {
+//   getPdfURL
+// }
